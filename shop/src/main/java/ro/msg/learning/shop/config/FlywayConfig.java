422acb8a234278ff7d8683926aff9dc569ac65a7
Bogdan Alexandru Ciubuc <Bogdan.Alexandru.Ciubuc@msg.group>
2019-07-16 13:35:48 +0300

    Configured h2 and Flyway and created the database tables

---
+++ shop/src/main/java/ro/msg/learning/shop/config/FlywayConfig.java
@@ -0,0 +1,13 @@
+package ro.msg.learning.shop.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+
+public class FlywayConfig {
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+
+        return Flyway.configure().dataSource("jdbc:h2:file:~/shop", "sa", "smth").load();
+    }
+}
