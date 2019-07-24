cb2f94ac16959299fa4fb0fb4035471d033cedd5
Bogdan Alexandru Ciubuc <Bogdan.Alexandru.Ciubuc@msg.group>
2019-07-24 15:25:35 +0300

    Second code review of chapter 5

--- shop/src/test/java/ro/msg/learning/shop/integration/IntegrationTests.java
+++ shop/src/test/java/ro/msg/learning/shop/integration/IntegrationTests.java
@@ -1,25 +1,42 @@
 package ro.msg.learning.shop.integration;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import ro.msg.learning.shop.repository.*;
 import ro.msg.learning.shop.strategy.MostAbundantStrategy;
 import ro.msg.learning.shop.strategy.SingleLocationStrategy;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
+@Transactional
 public class IntegrationTests {
 
     @Autowired
     LocationRepository locationRepository;
+
     @Autowired
     StockRepository stockRepository;
+
     @Autowired
     ProductRepository productRepository;
+
     @Autowired
     OrderRepository orderRepository;
+
     @Autowired
     OrderDetailRepository orderDetailRepository;
+
     @Autowired
     CustomerRepository customerRepository;
+
     @Autowired
     AddressRepository addressRepository;
 
